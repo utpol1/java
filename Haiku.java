2120e2bfa0dc2f473290f6d482a0ea93a4ceec52
jmfcool <me@jmfcool.com>
2013-04-05 23:59:08 -0700

    Add Haiku.java

---
+++ Haiku.java
@@ -0,0 +1,17 @@
+//**************************************************
+// Haiku.java : program prints a message
+// Inputs : none
+// Outputs : msgs
+// Written by   : Jeremiah Faria
+// Last Modified : Jan 27, 2012
+//**************************************************
+
+public class Haiku 
+{
+	public static void main (String[] args)  
+	{
+		System.out.print("Learn to");
+		System.out.println(" write haiku");
+		System.out.println("there are rules for syllables \ncompiling the rest.");
+	}
+}
