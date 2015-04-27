40edd67b836d37a82e6f945c0fd94cec327aab4a
Jeremiah Faria <jmfcool@me.com>
2015-04-27 14:25:13 -0700

    Update Serialize.java

--- serialize/Serialize.java
+++ serialize/Serialize.java
@@ -4,50 +4,30 @@ public class Serialize
 {
 	public static void main(String[] args)
 	{
+		String x = "www.jeremiahfaria.com";
+
                 System.out.print("\n" + "Serialize a String:" + "\n");
-                Serialize.serialize();
+                Serialize.serialize(x);
    	
                 System.out.print("\n" + "Deserialize a String:" + "\n");
-                Serialize.deserialize();
+                Serialize.deserialize(x);
+
+		System.out.print("\n");
 	}
-	public static void serialize()
+	public static void serialize(String data)
 	{
-		String[] alpha = {"bravo","charlie","delta"};
 		try
 		{
-			ByteArrayOutputStream echo = new ByteArrayOutputStream();
-			ObjectOutputStream foxtrot = new ObjectOutputStream(echo);
-			foxtrot.writeObject(alpha);
-			foxtrot.flush();
-			System.out.print(echo.toString() + "\n\n");
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ObjectOutputStream oos = new ObjectOutputStream(baos);
+			oos.writeObject(data);
+			oos.flush();
+			System.out.print(baos.toString() + "\n");
 		}
 		catch (Exception e)
 		{
 			System.out.print(e + "\n");
 		}
 	}
-	public static void deserialize()
-	{
-		String golf = "";
-		String[] alpha = {"bravo","charlie","delta"};
-		try
-		{
-			ByteArrayOutputStream echo = new ByteArrayOutputStream();
-			ObjectOutputStream foxtrot = new ObjectOutputStream(echo);
-			foxtrot.writeObject(alpha);
-			foxtrot.flush();
-			golf = echo.toString();
-			
-			byte[] b = golf.getBytes();
-			ByteArrayInputStream bi = new ByteArrayInputStream(b);
-			ObjectInputStream si = new ObjectInputStream(bi);
-			//MyObject obj = (MyObject) si.readObject();
-			
-			System.out.print(si.readObject() + "\n\n");
-		}
-		catch (Exception e)
-		{
-			System.out.print(e + "\n");
-		}
-	}
+	public static void deserialize(String data) {}
 }
